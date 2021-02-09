428d7c8841084865ab38f436cda610fe0ffb3154
emil.krslak <krslak_emil@hotmail.com>
2021-02-09 16:34:50 +0100

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AboutSection from './components/AboutSection';
 import AboutUs from './pages/AboutUs';
+import GlobalStyles from './components/GlobalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <AboutUs />
     </div>
   );
