a95c91f973ea314ae3beaa00957780e293de70e9
emil.krslak <krslak_emil@hotmail.com>
2021-02-09 17:37:45 +0100

    Changed index.html and Started with OurWork

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import AboutSection from './components/AboutSection';
 import AboutUs from './pages/AboutUs';
 import GlobalStyles from './components/GlobalStyles';
 import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
