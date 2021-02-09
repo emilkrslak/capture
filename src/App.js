517fb469b0267a761a20ce1afbe8adeca7f88fde
emil.krslak <krslak_emil@hotmail.com>
2021-02-09 17:11:42 +0100

    Created Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AboutSection from './components/AboutSection';
 import AboutUs from './pages/AboutUs';
 import GlobalStyles from './components/GlobalStyles';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <AboutUs />
     </div>
   );
